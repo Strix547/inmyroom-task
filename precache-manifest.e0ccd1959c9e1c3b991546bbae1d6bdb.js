42deeb04525e98723c5aea0a9f3eeedc426a4537
Alexander <alexxx547@gmail.com>
2020-03-14 21:37:23 +0300

    Updates

---
+++ precache-manifest.e0ccd1959c9e1c3b991546bbae1d6bdb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defc3f223c7669c9590f81fcfdfbaec0",
+    "url": "/inmyroom-task/index.html"
+  },
+  {
+    "revision": "77faa70e936f441fbf84",
+    "url": "/inmyroom-task/static/css/main.da20972e.chunk.css"
+  },
+  {
+    "revision": "c1caf3d94007b02887b9",
+    "url": "/inmyroom-task/static/js/2.5a7cf0e5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/inmyroom-task/static/js/2.5a7cf0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77faa70e936f441fbf84",
+    "url": "/inmyroom-task/static/js/main.53946ef5.chunk.js"
+  },
+  {
+    "revision": "a804395c5d29a7205ca5",
+    "url": "/inmyroom-task/static/js/runtime-main.0067050d.js"
+  },
+  {
+    "revision": "92c1560a3fe51759023b87709cbf7de2",
+    "url": "/inmyroom-task/static/media/cart.92c1560a.svg"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/inmyroom-task/static/media/heart.8b0126c6.svg"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/inmyroom-task/static/media/montserrat-bold.9c71d42b.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/inmyroom-task/static/media/montserrat-medium.d815b0a2.otf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/inmyroom-task/static/media/montserrat-semibold.bb3740d3.otf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/inmyroom-task/static/media/proximanova-bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "386c8ff06aaa9d3ea14528a5703b6ec5",
+    "url": "/inmyroom-task/static/media/proximanova-regular.386c8ff0.ttf"
+  },
+  {
+    "revision": "df8c626474a73ab7a8b511655597c7c4",
+    "url": "/inmyroom-task/static/media/proximanova-semibold.df8c6264.ttf"
+  },
+  {
+    "revision": "6dfffeda89b8582c3d54c8ab1bfd0653",
+    "url": "/inmyroom-task/static/media/sofa.6dfffeda.png"
+  }
+]);
